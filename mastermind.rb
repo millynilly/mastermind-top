46242d0dfb35c6e239b64990f1e9e531ac1aef6c
Amy <redcardigan@live.com>
2022-05-16 21:08:06 +0100

    Refactor play method into Game class

--- mastermind.rb
+++ mastermind.rb
@@ -2,30 +2,6 @@
 
 require_relative 'game.rb'
 
-
-def play
-  game = Game.new
-  
-  until game.win? || game.guesses == game.GUESS_MAX
-
-    game.get_guess
-    game.get_score
-    game.display_score
-    game.increment_guesses
-
-  end
-
-  game.display_result
-  puts 'Play again?'
-  play if ['y', 'Y'].include?(gets.chomp)
-
+while Game.new.play do
+  Game.new.play
 end
-
-play
-
-
-
-
-
-
-
